98700f05937de1086e2ed04681bbd71c716f2e07
Deirror <alexmailbg@gmail.com>
2025-06-14 17:42:02 +0300

    Add first version of env

---
+++ env/env.go
@@ -0,0 +1,65 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	DEV     = "dev"
+	PROD    = "prod"
+	STAGING = "staging"
+)
+
+// Loads environment variables from the given files
+// It is intented to be used in development mode
+func LoadEnv(filenames ...string) error {
+	if err := godotenv.Load(filenames...); err != nil {
+		return fmt.Errorf("error loading .env file: %v", err)
+	}
+	return nil
+}
+
+// A wrapper func around os.Getenv, but handles error with more text
+func GetEnv(key string) (string, error) {
+	val := os.Getenv(key)
+
+	if val == "" {
+		return "", fmt.Errorf("environment variable %s not set", key)
+	}
+
+	return val, nil
+}
+
+// Environment variable MODE must be set in .env to call this function
+// Can be used for dev, prod and staging environments
+func GetMode() (string, error) {
+	mode, err := GetEnv("MODE")
+	if err != nil {
+		return "", err
+	}
+
+	// Make it case-insensitive
+	mode = strings.ToLower(mode)
+
+	switch mode {
+	case DEV:
+	case PROD:
+	case STAGING:
+		return mode, nil
+	}
+
+	return "", fmt.Errorf("invalid environment mode: %s", mode)
+}
+
+// A wrapper func aroung os.Environ, handling errors more precisely
+func GetAllEnv() ([]string, error) {
+	kvps := os.Environ()
+	if len(kvps) == 0 {
+		return nil, fmt.Errorf("no environment variables found")
+	}
+	return kvps, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Deirror/dutils
+
+go 1.24.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
