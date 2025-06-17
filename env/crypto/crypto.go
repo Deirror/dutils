5a5606d8245857a91834cb861f802d818ac97910
Deirror <deirror.dev@gmail.com>
2025-06-17 11:34:48 +0300

    Add hashing algorithms in crypto package

---
+++ env/crypto/crypto.go
@@ -0,0 +1,15 @@
+package cyrpto
+
+import "golang.org/x/crypto/bcrypt"
+
+// Using for hashing passwords using salt.
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed), err
+}
+
+// Checks if passwords have equal hash
+func CheckPasswordHash(hash, password string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil, err
+}
