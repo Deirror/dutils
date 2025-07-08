42ccd67f54fd45f72628b09ab8bf22b7bbd0d932
Deirror <deirror.dev@gmail.com>
2025-07-08 13:05:28 +0300

    Add wrapper func for logging with slog

--- logger/logger.go
+++ logger/logger.go
@@ -1,8 +1,12 @@
 package logger
 
 import (
+	"context"
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/Deirror/dutils/api"
 )
 
 // Inits the go standart logger, based on env mode.
@@ -15,4 +19,16 @@ func InitLogger(mode string) *slog.Logger {
 	}
 	logger := slog.New(h)
 	return logger
+}
+
+// Wrapper func, used for dev/prod-ready logging - based on the slog.LevelInfo
+func LogWithAttrs(ctx context.Context, logger *slog.Logger, funcName string, begin time.Time, err error, extraAttrs ...slog.Attr) {
+	attrs := []slog.Attr{
+		slog.Any(api.ReqIDKey, ctx.Value(api.ReqIDKey)),
+	}
+	attrs = append(attrs, extraAttrs...)
+	attrs = append(attrs, slog.Duration("took", time.Since(begin)))
+	attrs = append(attrs, slog.Any("error", err))
+
+	logger.LogAttrs(ctx, slog.LevelInfo, funcName, attrs...)
 }
