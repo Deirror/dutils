fb3f90e73b67a1c43ed683d1c28a1138d8d49a9d
Deirror <deirror.dev@gmail.com>
2025-07-29 19:16:03 +0300

    Fix minor change - header set text/html

--- templ/components/components.go
+++ templ/components/components.go
@@ -8,5 +8,6 @@ import (
 
 // Writes rendered templ component to the response.
 func SendTempl(w http.ResponseWriter, r *http.Request, comp templ.Component) {
+	w.Header().Set("Content-Type", "text/html")
 	templ.Handler(comp).ServeHTTP(w, r)
 }
