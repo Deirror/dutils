33365c105e267e84e79f091a5d095c80fdcfec35
Aleksandar Angelov <deirror.dev@gmail.com>
2026-01-28 21:27:22 +0200

    feat: add std server wrapper

--- transport/http/server/http_server.go
+++ transport/http/server/http_server.go
@@ -1,12 +1,39 @@
 package httpsrv
 
 import (
+	"context"
+	"log/slog"
 	"net/http"
 	"strings"
 )
 
+type Server struct {
+	log *slog.Logger
+
+	srv *http.Server
+}
+
+func New(cfg *Config, log *slog.Logger, h http.Handler) *Server {
+	return &Server{
+		log: log,
+		srv: NewStdServer(cfg, h),
+	}
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	s.log.Info("HTTP Server is starting", slog.String("addr", s.srv.Addr))
+
+	return s.srv.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	s.log.Info("HTTP Server is shutting down")
+
+	return s.srv.Shutdown(ctx)
+}
+
 // New creates a standard HTTP server using the provided configuration and handler.
-func New(cfg *Config, h http.Handler) *http.Server {
+func NewStdServer(cfg *Config, h http.Handler) *http.Server {
 	addr := cfg.Port
 	if !strings.HasPrefix(cfg.Port, ":") {
 		addr = ":" + cfg.Port
